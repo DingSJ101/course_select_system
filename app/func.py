2e232888cbcc917a04a8c8e45e8469c5d4621530
Your Name <you@example.com>
2022-08-09 21:34:48 +0800

    update

--- app/func.py
+++ app/func.py
@@ -65,21 +65,6 @@ def time_overlap_check(box1,box2):
                 return True
     return False
 
-## classtime_judge test --------------
-# StudentNum = '19000000'
-# student = Student.query.filter_by(StudentNum = StudentNum).first()
-# classnum = '00814022_1001'
-# cla = Class.query.filter_by(ClassNum=classnum)
-# test = "一5-6   四7-8 全英语"
-# test2 = "二3-6 123 四5-6"
-# timebox=[[0 for j in range(14)] for i in range(8)]
-# timebox2=[[0 for j in range(14)] for i in range(8)]
-# time_transform(test,timebox)
-# time_transform(test2,timebox2)
-# # print(time_overlap_check(timebox,timebox2))
-# print(classtime_judge(test,student.Classes))
-## -------------------------------------
-
 def deptname2deptnum(deptname):
     if deptname in deptnames :
         return deptnums[deptnames.index(deptname)]
@@ -105,7 +90,6 @@ def load_student_from_file(filepath):
     
     if filetype == 'txt':
         f = open(filepath,encoding = "utf-8")
-        
         for i , line in enumerate(f.readlines()):
             order , name , dept , major  =  re.split(r' +|\n|\t',line)[:4]
             deptnum = deptname2deptnum(dept)
