5203bf2ec205677c71e781ec002a9483c540c2df
Your Name <you@example.com>
2022-08-11 17:34:52 +0800

    修改配置

--- app/config.py
+++ app/config.py
@@ -6,24 +6,23 @@ class Config(object):
     #SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root@localhost:3306/course_select_system?charset=utf8'
     # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://gaussdb:123@QWEasd@175.24.167.6:15432/css'
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://gaussdb:123@QWEasd@122.9.68.170:15432/css'
-#    SQLALCHEMY_TRACK_MODIFICATIONS = False
-# 动态追踪修改设置，如未设置只会提示警告
-    SQLALCHEMY_TRACK_MODIFICATIONS = True
-    SQLALCHEMY_ECHO = True
-    # 开启代码自动提交功能 [不推荐开启]
+
+    # SQLALCHEMY_TRACK_MODIFICATIONS = True
+    # SQLALCHEMY_ECHO = True
+    # 锟斤拷锟斤拷锟斤拷锟斤拷锟皆讹拷锟结交锟斤拷锟斤拷 [锟斤拷锟狡硷拷锟斤拷锟斤拷]
     # SQLALCHEMY_COMMIT_ON_TEARDOWN = True
 class DevelopmentConfig(Config):
-    # 开发模式
-    DEBUG = True
+    # 锟斤拷锟斤拷模式
+    DEBUG = False
 class ProductionConfig(Config):
-    # 生产环境
+    # 锟斤拷锟斤拷锟斤拷锟斤拷
     pass
 
 config_map = {
    "develop": DevelopmentConfig,
     "product": ProductionConfig
 }
-# 跨域
+# 锟斤拷锟斤拷
 cors = {
     "Access-Control-Allow-Origin": "*"
 }
